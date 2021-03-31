f0adac63479c96143e700f3dd29fdaaf342103a2
tiagomaie <vr393028@hpc-head.cm.cluster>
2021-03-31 14:46:47 +0200

    adding examples of how to run the pipeline+viz and configure colors

---
+++ run_example.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+### Job name
+#SBATCH -J proj_ex
+
+### Set logs, remember to create logs dir
+#SBATCH -e logs/log_%j.log
+#SBATCH -o logs/log_%j.log
+
+### Time to execute, e. g. 15 min 30 sec
+#SBATCH -t 96:00:00
+
+### Job memory needs per node, e. g. 1 GB
+#SBATCH --mem=180G
+
+### OpenMP threads
+#SBATCH --cpus-per-task=16
+
+################################################################
+# PATH
+if [ -r /usr/local_host/etc/bashrc ]; then
+   . /usr/local_host/etc/bashrc
+fi
+
+export PATH=/usr/bin:$PATH
+export PATH=/usr/local_host/bin:$PATH
+################################################################
+module load scRNA
+################################################################
+
+# could also define it here instead of taking it as an arg
+proj_name="$1"
+# data dir (where your results will be saved)
+data_path="/data/EXAMPLE/exp/scRNA/some_project/scrna_seurat_pipeline_results"
+
+date
+## 50 cores run, future memory
+Rscript data_factory.R -n 16 \
+  --MaxMemMega=180000 \
+  -c "./conf/config_${proj_name}.R" \
+  -s "${data_path}/save_${proj_name}" \
+  -e "${data_path}/charts_${proj_name}" \
+  -a seurat_clusters \
+  -r 0.2
+
+date
