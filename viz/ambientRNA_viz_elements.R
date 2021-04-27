66948ed06430480bf4d30854e1803497599cdedb
grasshoffm <49937348+grasshoffm@users.noreply.github.com>
2021-04-27 18:51:37 +0200

    Update ambientRNA_viz_elements.R

--- viz/ambientRNA_viz_elements.R
+++ viz/ambientRNA_viz_elements.R
@@ -1,23 +1,20 @@
 ####################################################
 # ambient RNA
 ####################################################
-## if load celda here, Seurat methods would be replaced by old version
-#suppressPackageStartupMessages(library(celda))
-
-#suppressPackageStartupMessages(library(ggplot2))
-#suppressPackageStartupMessages(library(dplyr))
-#suppressPackageStartupMessages(library(Seurat))
-
 if(identical(cluster,"singleton")){
   scrna <- readRDS(file=file.path(savedir, "scrna_phase_singleton.Rds"))
 }else{
-  scrna <- readRDS(file = file.path(savedir, "scrna_phase_preprocess.Rds"))
+  scrna <- readRDS(file = file.path(savedir, "scrna_phase_clustering.Rds"))
 }
 plt <- list()
 
 plt[[1]] <- FeaturePlot(scrna, features = "AmbientRNA") + ggtitle(label = "Ambient RNA Contamintaion")
 
 plt[[2]] <- VlnPlot(object = scrna, features = "AmbientRNA", group.by = "decontX_clusters", pt.size=0.1)
+
+plt[[3]] <- VlnPlot(object = scrna, features = "AmbientRNA", group.by = "decontX_clusters")
+
+plt[[4]] <- VlnPlot(object = scrna, features = "AmbientRNA_Harmony", group.by = "decontX_clusters_harmony")
 
 save_ggplot_formats(
   plt=plt[[1]],
@@ -31,6 +28,18 @@ save_ggplot_formats(
   plt_name="ambient_rna_vln",
   width=9, height=7
 )
+save_ggplot_formats(
+  plt=plt[[3]],
+  base_plot_dir=report_plots_folder,
+  plt_name="ambient_rna_vln",
+  width=9, height=7
+)
+save_ggplot_formats(
+  plt=plt[[4]],
+  base_plot_dir=report_plots_folder,
+  plt_name="ambient_rna_vln_harmony",
+  width=9, height=7
+)
 
 meta <- scrna@meta.data
 stSample <- meta %>%
@@ -38,6 +47,8 @@ stSample <- meta %>%
   summarise(
     ambientRNA.Mean=mean(AmbientRNA),
     ambientRNA.Median=median(AmbientRNA),
+    ambientRNA.Harmony.Mean=mean(AmbientRNA_Harmony),
+    ambientRNA.Harmony.Median=median(AmbientRNA_Harmony)
   )
 saveRDS(stSample,file.path(report_tables_folder,"ambientRNA_postfilter.RDS"))
 
